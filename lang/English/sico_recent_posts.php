3f0b3ba09d8bb3f84606c17a5c460544ccfc820c
Simon Corless <simon@sico.co.uk>
2011-11-27 20:32:22 +0000

    Renamed language file. Removed time translations as now using forum based time functions.

---
+++ lang/English/sico_recent_posts.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * English Language file for sico_recent_posts
+ *
+ * @copyright Copyright (C) 2011 Simon Corless
+ * @license http://www.gnu.org/licenses/gpl.html Released under GPL license version 3 or any later version
+ * @package sico_recent_posts
+ */
+
+if (!defined('FORUM')) die();
+
+$lang_latest_posts = array(
+    //The real thing
+    'Latest posts' => 'Latest posts',
+    'New' => '<b>New topic:</b>',
+    'Reply' => '<b>Reply to:</b>',
+);
